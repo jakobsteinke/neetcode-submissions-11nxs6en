e9f11bd3665fa1ee5e8bddbce8a301ab9f0ff18c
Jakob <145787197+jakobsteinke@users.noreply.github.com>
2026-09-02 03:56:19 +0200

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        metaData = [(position[i], speed[i]) for i in range(len(position))]
+        metaData.sort(key=lambda x: x[0])
+        stack = []
+        for i in range(len(position)):
+            positionI, speedI = metaData[i]
+            stack.append((target - positionI) / speedI)
+        fleetCounter = 0
+        while stack:
+            time = stack.pop()
+            fleetCounter += 1
+            while stack and stack[-1] <= time:
+                stack.pop()
+        return fleetCounter
+
+
